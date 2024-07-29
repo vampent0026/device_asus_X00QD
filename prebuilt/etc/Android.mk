4f01eeffca9d403731338c295b3246846924d555
vampent0026 <bencena0026@gmail.com>
2024-07-29 23:10:06 +0800

    Update Android.mk

--- prebuilt/etc/Android.mk
+++ prebuilt/etc/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := apns-conf
 LOCAL_SRC_FILES := prebuilt/etc/apns-conf.xml
-LOCAL_MODULE_CLASS := etc
+LOCAL_MODULE_CLASS := prebuilt_etc
 LOCAL_MODULE_OWNER := X00QD
 include $(BUILD_PREBUILT)
