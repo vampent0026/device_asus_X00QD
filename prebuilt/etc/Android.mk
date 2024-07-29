26ae123b31965966c30e348b6d6d1715ae670cdd
vampent0026 <bencena0026@gmail.com>
2024-07-29 23:01:33 +0800

    updated code

---
+++ prebuilt/etc/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := apns-conf
+LOCAL_SRC_FILES := prebuilt/etc/apns-conf.xml
+LOCAL_MODULE_CLASS := etc
+LOCAL_MODULE_OWNER := X00QD
+include $(BUILD_PREBUILT)
