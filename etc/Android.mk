6c16a4e4a0d1fe3cf875c24ac0141e8a02e992be
vampent0026 <bencena0026@gmail.com>
2024-08-01 23:33:43 +0800

    removed apn config

--- etc/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := apns-conf
-LOCAL_SRC_FILES := etc/apns-conf.xml
-LOCAL_MODULE_CLASS := etc
-LOCAL_MODULE_OWNER := X00QD
-include $(BUILD_PREBUILT)
